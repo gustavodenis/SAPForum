114f587be2d9443e15b1b4da4e61d1a1e0ad1dc0
gustavodenis <gustavo@gdtek.net>
2014-01-09 15:26:54 -0200

    ajuste qr code

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,10 @@ sapForumApp.prototype = function () {
             _login = true;
             $.mobile.changePage('#home', { transition: 'flip' });
         }
+
+        $('#scanQR').on('click', function () {
+            sapForumApp._scanQR();
+        });
     },
 
     _initpointsDetail = function () {
@@ -55,6 +59,33 @@ sapForumApp.prototype = function () {
 
     _initluluPage = function () {
     },
+
+    _scanQR = function () {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan(function (result) {
+
+            alert("We got a barcode\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+
+            console.log("Scanner result: \n" +
+                 "text: " + result.text + "\n" +
+                 "format: " + result.format + "\n" +
+                 "cancelled: " + result.cancelled + "\n");
+            document.getElementById("info").innerHTML = result.text;
+            console.log(result);
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+        }, function (error) {
+            console.log("Scanning failed: ", error);
+        });
+    }
 
     _checkConnection = function() {
         var networkState = navigator.connection.type;
